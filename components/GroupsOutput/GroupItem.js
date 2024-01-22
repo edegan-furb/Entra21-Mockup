7b4e732a51ac1dac6ddc491da6f08d558f93b509
Jvanz <j.vanz1108@gmail.com>
2024-01-22 17:15:05 -0300

    Criando informações dos grupos

--- components/GroupsOutput/GroupItem.js
+++ components/GroupsOutput/GroupItem.js
@@ -2,9 +2,13 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { Colors } from "../../constants/styles";
+import { Ionicons, Foundation } from '@expo/vector-icons';
+import { GroupsContext } from "../../store/groups-context";
 
 function GroupItem({ id, title }) {
   const navigation = useNavigation();
+
+  GroupsContext
 
   function groupPressHandler() {
     navigation.navigate("GroupScreen", {
@@ -18,8 +22,24 @@ function GroupItem({ id, title }) {
       style={({ pressed }) => pressed && styles.pressed}
     >
       <View style={styles.groupItem}>
-        <View>
-          <Text style={[styles.textBase, styles.title]}>{title}</Text>
+        <View style={styles.content}>
+          <View style={styles.titleContainer}>
+            <Text style={[styles.textBase, styles.title]}>{title}</Text>
+          </View>
+          <View style={styles.infGroupsContainer}>
+            <View style={styles.infGroups}>
+              <Ionicons name="people" color={'#fff'} size={17} />
+              <Text style={styles.infTitleGroup}>
+                4
+              </Text>
+            </View>
+            <View style={styles.infGroups}>
+              <Foundation name="clipboard-pencil" color={'#fff'} size={17}/>
+              <Text style={styles.infTitleGroup}>
+                4
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     </Pressable>
@@ -34,25 +54,44 @@ const styles = StyleSheet.create({
   },
   groupItem: {
     width: '100%',
-    height: 80,
-    marginVertical: 8,
+    height: 100,
     backgroundColor: Colors.primary800,
-    justifyContent: "space-between",
-    borderRadius: 9,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    marginVertical: 8,
+    borderRadius: 5,
     elevation: 3,
     shadowColor: Colors.primary100,
     shadowRadius: 4,
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.4,
-    justifyContent: "center"
+  },
+  content: {
+    paddingLeft: '10%'
+  },
+  titleContainer: {
+    width: '50%',
+    height: '55%',
   },
   textBase: {
     color: Colors.primary100,
   },
   title: {
     fontSize: 16,
-    marginBottom: 4,
     fontWeight: "bold",
-    marginLeft: 15
   },
+  infGroupsContainer: {
+    width: '50%',
+    flexDirection: "row",
+  },
+  infGroups: {
+    width: '50%',
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5
+  },
+  infTitleGroup: {
+    color: '#fff',
+    fontSize: 13
+  }
 });
