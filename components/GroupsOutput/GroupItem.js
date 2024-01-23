8a68351cb862adfaea1b86a380575616a34f8d8f
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2024-01-23 15:25:28 -0300

    componente de grafico donnud

--- components/GroupsOutput/GroupItem.js
+++ components/GroupsOutput/GroupItem.js
@@ -4,6 +4,10 @@ import { useNavigation } from "@react-navigation/native";
 import { Colors } from "../../constants/styles";
 import { Ionicons, Foundation } from '@expo/vector-icons';
 import { GroupsContext } from "../../store/groups-context";
+import CompletedTasks from "../graphComponents/completedTasks";
+
+
+
 
 function GroupItem({ id, title }) {
   const navigation = useNavigation();
@@ -23,23 +27,26 @@ function GroupItem({ id, title }) {
     >
       <View style={styles.groupItem}>
         <View style={styles.content}>
-          <View style={styles.titleContainer}>
-            <Text style={[styles.textBase, styles.title]}>{title}</Text>
-          </View>
-          <View style={styles.infGroupsContainer}>
-            <View style={styles.infGroups}>
-              <Ionicons name="people" color={'#fff'} size={17} />
-              <Text style={styles.infTitleGroup}>
-                4
-              </Text>
+          <View style={styles.container}>
+            <View style={styles.titleContainer}>
+              <Text style={[styles.textBase, styles.title]}>{title}</Text>
             </View>
-            <View style={styles.infGroups}>
-              <Foundation name="clipboard-pencil" color={'#fff'} size={17}/>
-              <Text style={styles.infTitleGroup}>
-                4
-              </Text>
+            <View style={styles.infGroupsContainer}>
+              <View style={styles.infGroups}>
+                <Ionicons name="people" color={'#fff'} size={17} />
+                <Text style={styles.infTitleGroup}>
+                 4
+                </Text>
+              </View>
+              <View style={styles.infGroups}>
+                <Foundation name="clipboard-pencil" color={'#fff'} size={17}/>
+                <Text style={styles.infTitleGroup}>
+                  4
+               </Text>
+              </View>  
             </View>
           </View>
+        <CompletedTasks concludedTasks={60} pendingTasks={40} />  
         </View>
       </View>
     </Pressable>
@@ -52,12 +59,16 @@ const styles = StyleSheet.create({
   pressed: {
     opacity: 0.75,
   },
+  container: {
+    backgroundColor: 'red'
+  },
   groupItem: {
     width: '100%',
     height: 100,
     backgroundColor: Colors.primary800,
     justifyContent: "center",
     alignItems: "flex-start",
+    flexDirection: 'row',
     marginVertical: 8,
     borderRadius: 5,
     elevation: 3,
@@ -67,11 +78,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.4,
   },
   content: {
-    paddingLeft: '10%'
+    paddingLeft: '10%',
+    backgroundColor: '#333',
+    flexDirection: 'row'
   },
   titleContainer: {
     width: '50%',
     height: '55%',
+    backgroundColor: '#999'
   },
   textBase: {
     color: Colors.primary100,
@@ -83,6 +97,7 @@ const styles = StyleSheet.create({
   infGroupsContainer: {
     width: '50%',
     flexDirection: "row",
+    backgroundColor: '#666'
   },
   infGroups: {
     width: '50%',
