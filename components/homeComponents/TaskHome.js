acfd645dafe1bd399b26370f9b95bfaf0b574c84
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2024-01-30 16:50:41 -0300

    Componete de task na Home.

---
+++ components/homeComponents/TaskHome.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp
+} from "react-native-responsive-screen";
+
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/styles";
+import * as Progress from 'react-native-progress';
+
+export default function TaskHome({ deadline, taskName, groupName, taskProgress}) {
+    return(
+        <View style={styles.container}>
+            <View>
+                <Text>{deadline}</Text>
+            </View>
+
+            <View style={styles.taskNameContainer}>
+                <Ionicons
+                    size={hp('7%')} 
+                    name="reader-outline"
+                    color={Colors.primary600}
+                />
+                <View>
+                    <Text style={styles.taskName}>{taskName}</Text>
+                    <Text style={styles.groupName}>{groupName}</Text>
+                </View>
+            </View>
+
+            <View style={styles.progressContainer}>
+                <Text style={styles.ProgressText}>Progress:</Text>
+                <View style={styles.barContainer}>
+                    <Progress.Bar 
+                        progress={taskProgress}
+                        color={Colors.primary600} 
+                        width={wp('25%')} />
+                    <Text> { (taskProgress) * 100 }%</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: hp('22%'),
+        height: hp('22%'),
+        margin: wp('2%'),
+        padding: '3%',
+        borderWidth: 1,
+        borderRadius: 20,
+        backgroundColor: '#fff'
+    },
+    taskNameContainer: {
+        marginTop: '5%',
+        flexDirection: 'row'
+    },
+    ProgressText: {
+        color: Colors.neutral1100
+    },
+    taskName: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        color: Colors.neutral1100
+    },
+    groupName: {
+        fontSize: 12,
+        color: Colors.neutral1100
+    },
+    barContainer:{
+        flexGrow: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    progressContainer: {
+        marginTop: '5%'
+    }
+})
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -10,7 +10,7 @@ import CompletedTasks from '../components/homeComponents/graphComponents/Complet
 import TotalTasks from "../components/homeComponents/graphComponents/TotalTasks";
 import MissedDeadlines from "../components/homeComponents/graphComponents/MissedDeadlines";
 import NumberOfGroups from "../components/homeComponents/graphComponents/NumberOfGroups";
-import ProgrssionGraph from "../components/homeComponents/graphComponents/ProgressionGraph";
+import TaskHome from "../components/homeComponents/TaskHome";
 
 import HiComp from "../components/homeComponents/HiComp";
 import WelcomeComp from "../components/homeComponents/WelcomeComp";
@@ -53,25 +53,26 @@ function WelcomeScreen({ navigation, route }) {
 
       <View style={styles.container}> 
       
-      <View style={styles.containerRow1}>
-        <WelcomeComp onPress={goToGroups}/>
-      </View>
-        
-
-        <TotalTasks total={99}/>
-        <View style={styles.containerRow2}>
-        
-          <CompletedTasks concludedTasks={70} pendingTasks={30}/>
-
-          <View>
-            <MissedDeadlines number={10}/>
-            <NumberOfGroups number={5}/>
-          </View>
-        
+        <View style={styles.containerRow1}>
+          <WelcomeComp onPress={goToGroups}/>
         </View>
         
-        <View style={styles.graphContainer}>
-          <ProgrssionGraph/>        
+          <TotalTasks total={99}/>
+
+          <View style={styles.containerRow2}>
+        
+            <CompletedTasks concludedTasks={70} pendingTasks={30}/>
+
+            <View>
+              <MissedDeadlines number={10}/>
+              <NumberOfGroups number={5}/>
+            </View>
+        
+          </View>
+
+        <View style={styles.tasksContainer}>
+          <TaskHome deadline={'Jan, 31 2024'} taskName={"Mobile app"} groupName={'Clube da luta'} taskProgress={0.8}/>
+          <TaskHome/>
         </View>
 
       </View>
@@ -90,7 +91,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
-    padding: 32, 
+    padding: '5%', 
   },
   containerRow2: {
     flexDirection: "row",
@@ -103,12 +104,17 @@ const styles = StyleSheet.create({
     marginBottom: '8%',
     marginLeft: '5%',
   },
+  tasksContainer: {
+    width: '100%',
+    flex: 1,
+    alignItems: "center",
+    justifyContent:'center',
+    flexDirection: 'row',
+    flexWrap: "wrap",
+  },
   title: {
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 8,
-  },
-  graphContainer: {
-    marginTop: '10%'
   }
 });
