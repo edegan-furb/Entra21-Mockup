629774daabc0d52faf4ceda842ab903e367b963e
Edegan <eduardord324@gmail.com>
2024-02-05 16:53:10 -0300

    Changed light to be the default color theme

--- store/theme-context.js
+++ store/theme-context.js
@@ -29,7 +29,7 @@ const ThemeContext = createContext({
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('light');
 
     const toggleTheme = () => {
         setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
