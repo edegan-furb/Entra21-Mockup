2b81cb3a3ec7061903635db81124db00faff551f
Eduardo Degan <edegan@furb.br>
2024-02-16 00:36:00 -0300

    Ariel era Inocente

--- components/ManageTask/TaskForm.js
+++ components/ManageTask/TaskForm.js
@@ -12,11 +12,19 @@ import {
 } from "../../util/firebase/firestore/user";
 import {
   heightPercentageToDP as hp,
-  widthPercentageToDP as wp
+  widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 import TranslatedText from "../../store/language-context";
 
-function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTitle, isEditing, onPressDelete }) {
+function TaskForm({
+  submitButtonLabel,
+  onCancel,
+  onSubmit,
+  defaultValues,
+  pageTitle,
+  isEditing,
+  onPressDelete,
+}) {
   const [inputs, setInputs] = useState({
     title: {
       value: defaultValues ? defaultValues.title : "",
@@ -36,36 +44,34 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
       isValid: true,
     },
     objectives:
-      defaultValues && defaultValues.objectives ? defaultValues.objectives.map((obj) => ({
-        id: obj.id,
-        value: obj.value,
-        completed: obj.completed,
-        isValid: true,
-      }))
-      : 
-      [
-        {
-          id: generateUniqueId(),
-          value: "",
-          completed: false,
-          isValid: true,
-        },
-      ],
-    }
-  );
+      defaultValues && defaultValues.objectives
+        ? defaultValues.objectives.map((obj) => ({
+            id: obj.id,
+            value: obj.value,
+            completed: obj.completed,
+            isValid: true,
+          }))
+        : [
+            {
+              id: generateUniqueId(),
+              value: "",
+              completed: false,
+              isValid: true,
+            },
+          ],
+  });
   const [currentUserEmail, setCurrentUserEmail] = useState("");
 
   useEffect(() => {
     // Assuming fetchUsernameAndEmail is an async function that returns { email: userEmail }
     fetchUsernameAndEmail()
       .then((userDetails) => {
-          setCurrentUserEmail(userDetails.email);
-          console.log(currentUserEmail);
+        setCurrentUserEmail(userDetails.email);
+        console.log(currentUserEmail);
       })
       .catch((error) => {
         console.error("Error fetching user email:", error);
-      }
-    );
+      });
   }, []);
 
   useEffect(() => {
@@ -82,10 +88,20 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
         .catch((error) => {
           console.error("Failed to fetch email", error);
           // Handle error, possibly by setting state
-        }
-      );
+        });
     }
   }, [defaultValues, getEmailByUsername]);
+
+  useEffect(() => {
+    const initialDateValue = defaultValues
+      ? getFormattedDate(defaultValues.date, true)
+      : inputs.title.value;
+    setformDate(initialDateValue);
+    setInputs((currentInputs) => ({
+      ...currentInputs,
+      date: { value: initialDateValue, isValid: true },
+    }));
+  }, [defaultValues, inputs.title.value]);
 
   function inputChangeHandler(inputIdentifier, enteredValue, index = null) {
     if (inputIdentifier === "objectives") {
@@ -154,8 +170,7 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
     const descriptionIsValid = taskData.description.trim().length > 0;
     const designatedUserIsValid =
       taskData.designatedUser.includes("@") &&
-      taskData.designatedUser.toLowerCase() !== currentUserEmail
-    ;
+      taskData.designatedUser.toLowerCase() !== currentUserEmail;
     const objectivesAreValid = inputs.objectives.every(
       (objective) => objective.value.trim().length > 0
     );
@@ -205,19 +220,17 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
     !inputs.date.isValid ||
     !inputs.description.isValid ||
     !inputs.designatedUser.isValid ||
-    inputs.objectives.some((objective) => !objective.isValid)
-  ;
-
-  const [formDate, setformDate] = useState('');
+    inputs.objectives.some((objective) => !objective.isValid);
+  const [formDate, setformDate] = useState("");
 
   const formatDate = (input) => {
     if (!input) return input;
 
     // Formatar a data com traços
-    let formattedDate = '';
+    let formattedDate = "";
     for (let i = 0; i < input.length; i++) {
-      if ((i === 7 || i === 4) && input[i] !== '-') {
-        formattedDate += '-';
+      if ((i === 7 || i === 4) && input[i] !== "-") {
+        formattedDate += "-";
       }
       formattedDate += input[i];
     }
@@ -226,9 +239,13 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
   };
 
   const handleTextChange = (text) => {
-    setformDate(formatDate(text));
+    const formattedText = formatDate(text);
+    setformDate(formattedText);
+    setInputs((currentInputs) => ({
+      ...currentInputs,
+      date: { value: formattedText, isValid: true },
+    }));
   };
-
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -237,37 +254,37 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
         </View>
       </View>
       <View style={styles.form}>
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-        >
+        <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.contentInput}>
             <Input
               style={styles.rowTitle}
-              label={<TranslatedText enText={'Title'} ptText={'Título'}/>}
+              label={<TranslatedText enText={"Title"} ptText={"Título"} />}
               invalid={!inputs.title.isValid}
               textInputConfig={{
                 multiline: false,
                 onChangeText: inputChangeHandler.bind(this, "title"),
                 value: inputs.title.value,
-                maxLength: 25
+                maxLength: 25,
               }}
             />
             <Input
               style={styles.rowInput}
-              label={<TranslatedText enText={'Date'} ptText={'Data'}/>}
+              label={<TranslatedText enText={"Date"} ptText={"Data"} />}
               invalid={!inputs.date.isValid}
               textInputConfig={{
                 placeholder: "YYYY-MM-DD",
                 onChangeText: handleTextChange,
                 value: formDate,
                 maxLength: 10,
-                keyboardType: "number-pad"
+                keyboardType: "number-pad",
               }}
             />
           </View>
           <View style={styles.inputRow}>
             <Input
-              label={<TranslatedText enText={'Description'} ptText={'Descrição'}/>}
+              label={
+                <TranslatedText enText={"Description"} ptText={"Descrição"} />
+              }
               invalid={!inputs.description.isValid}
               textInputConfig={{
                 multiline: true,
@@ -279,21 +296,31 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
 
           <View style={styles.inputRow}>
             <Input
-              label={<TranslatedText enText={'Designed User by e-mail'} ptText={'Designação do usuário por e-mail'}/>}
+              label={
+                <TranslatedText
+                  enText={"Designed User by e-mail"}
+                  ptText={"Designação do usuário por e-mail"}
+                />
+              }
               invalid={!inputs.designatedUser.isValid}
               textInputConfig={{
                 multiline: false,
-                autoCapitalize: 'none',
+                autoCapitalize: "none",
                 onChangeText: inputChangeHandler.bind(this, "designatedUser"),
                 value: inputs.designatedUser.value,
               }}
             />
-          </View>  
+          </View>
           {inputs.objectives.map((objective, index) => (
             <View key={index} style={styles.inputObjectivesRow}>
               <Input
                 style={styles.objectivesInput}
-                label={<TranslatedText enText={`Objective ${index + 1}`} ptText={`Objetivo ${index + 1}`}/>}
+                label={
+                  <TranslatedText
+                    enText={`Objective ${index + 1}`}
+                    ptText={`Objetivo ${index + 1}`}
+                  />
+                }
                 invalid={!objective.isValid}
                 textInputConfig={{
                   multiline: false,
@@ -317,42 +344,55 @@ function TaskForm({ submitButtonLabel, onCancel, onSubmit, defaultValues, pageTi
           ))}
           <View style={styles.buttonAddObjective}>
             <Button mode="flat" onPress={addObjective}>
-              <TranslatedText enText={'Add Objective'} ptText={'Add Objetivo'}/>
+              <TranslatedText
+                enText={"Add Objective"}
+                ptText={"Add Objetivo"}
+              />
             </Button>
           </View>
         </ScrollView>
         {formIsInvalid && (
-          <TranslatedText 
-            enText={'Invalid input values - please check your entered data.'} 
-            ptText={'Valores de entrada inválidos - verifique os dados inseridos.'}
-            style={styles.errorText}
-          />
-        )} 
-        {!inputs.designatedUser.isValid && (
-          <TranslatedText 
-            enText={'You cannot assign a task to yourself.'} 
-            ptText={'Você não pode atribuir uma tarefa a si mesmo.'}
+          <TranslatedText
+            enText={"Invalid input values - please check your entered data."}
+            ptText={
+              "Valores de entrada inválidos - verifique os dados inseridos."
+            }
             style={styles.errorText}
           />
         )}
-        <View style={isEditing ? styles.buttonsContainerEdit : styles.buttonsContainer}>
-          <View style={isEditing ? styles.buttonContentEdit : styles.buttonContent}>
+        {!inputs.designatedUser.isValid && (
+          <TranslatedText
+            enText={"You cannot assign a task to yourself."}
+            ptText={"Você não pode atribuir uma tarefa a si mesmo."}
+            style={styles.errorText}
+          />
+        )}
+        <View
+          style={
+            isEditing ? styles.buttonsContainerEdit : styles.buttonsContainer
+          }
+        >
+          <View
+            style={isEditing ? styles.buttonContentEdit : styles.buttonContent}
+          >
             <Button mode="flat" onPress={onCancel}>
-              <TranslatedText enText={'Cancel'} ptText={'Cancelar'}/>
+              <TranslatedText enText={"Cancel"} ptText={"Cancelar"} />
             </Button>
-            <Button onPress={submitHandler}>
-              {submitButtonLabel}
-            </Button>
+            <Button onPress={submitHandler}>{submitButtonLabel}</Button>
           </View>
           {isEditing && (
-          <Pressable style={styles.deleteContainer} onPress={onPressDelete}>
-            <TranslatedText enText={'Delete task'} ptText={'Deletar tarefa'} style={styles.textButton}/>
-            <IconButton
-              icon="trash"
-              color={Colors.error500}
-              size={20}
-              onPress={onPressDelete}
-            />
+            <Pressable style={styles.deleteContainer} onPress={onPressDelete}>
+              <TranslatedText
+                enText={"Delete task"}
+                ptText={"Deletar tarefa"}
+                style={styles.textButton}
+              />
+              <IconButton
+                icon="trash"
+                color={Colors.error500}
+                size={20}
+                onPress={onPressDelete}
+              />
             </Pressable>
           )}
         </View>
@@ -365,15 +405,15 @@ export default TaskForm;
 
 const styles = StyleSheet.create({
   container: {
-    width: wp('90%'),
-    height: hp('75%'),
+    width: wp("90%"),
+    height: hp("75%"),
     backgroundColor: Colors.primary900,
     borderRadius: 20,
-    marginTop: '15%',
+    marginTop: "15%",
   },
   titleContainer: {
-    height: '15%',
-    width: '100%',
+    height: "15%",
+    width: "100%",
     backgroundColor: Colors.primary100,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
@@ -383,8 +423,8 @@ const styles = StyleSheet.create({
     borderTopEndRadius: 20,
     borderTopStartRadius: 20,
     borderBottomLeftRadius: 30,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -393,48 +433,48 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: Colors.primary100,
     textAlign: "center",
-  },  
+  },
   form: {
-    width: '100%',
-    height: '85%',
+    width: "100%",
+    height: "85%",
     paddingHorizontal: 10,
     flex: 1,
     borderTopEndRadius: 30,
     borderBottomEndRadius: 20,
     borderBottomStartRadius: 20,
-    backgroundColor: Colors.primary100
+    backgroundColor: Colors.primary100,
   },
   contentInput: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
-    marginTop: 5
+    marginTop: 5,
   },
   rowTitle: {
-    flex: 2
+    flex: 2,
   },
   rowInput: {
-    flex: 1
+    flex: 1,
   },
   scrollView: {
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   inputObjectivesRow: {
     flexDirection: "row",
     flex: 1,
-    alignItems: "flex-end"
+    alignItems: "flex-end",
   },
   objectivesInput: {
-    width: '80%'
+    width: "80%",
   },
   removeButton: {
-    height: '70%',
+    height: "70%",
   },
   buttonAddObjective: {
     padding: 10,
     height: 65,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   errorText: {
     textAlign: "center",
@@ -442,47 +482,47 @@ const styles = StyleSheet.create({
     margin: 8,
   },
   buttonsContainer: {
-    width: '100%',
-    height: '20%',
+    width: "100%",
+    height: "20%",
     alignItems: "center",
     justifyContent: "space-evenly",
     borderTopWidth: 2,
-    borderColor: Colors.primary900
+    borderColor: Colors.primary900,
   },
   buttonsContainerEdit: {
-    width: '100%',
-    height: '25%',
+    width: "100%",
+    height: "25%",
     alignItems: "center",
     justifyContent: "space-evenly",
     borderTopWidth: 2,
-    borderColor: Colors.primary900
+    borderColor: Colors.primary900,
   },
   buttonContent: {
-    height: '50%',
-    width: '85%',
+    height: "50%",
+    width: "85%",
     flexDirection: "row",
     gap: 10,
     alignItems: "center",
   },
   buttonContentEdit: {
-    height: '35%',
-    width: '85%',
+    height: "35%",
+    width: "85%",
     flexDirection: "row",
     gap: 10,
     alignItems: "center",
   },
   deleteContainer: {
-    height: '35%',
-    width: '50%',
+    height: "35%",
+    width: "50%",
     borderRadius: 10,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: Colors.primary900
+    backgroundColor: Colors.primary900,
   },
   textButton: {
     color: Colors.primary100,
     fontWeight: "bold",
-    paddingLeft: 15
-  }
+    paddingLeft: 15,
+  },
 });
